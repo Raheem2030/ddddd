ae8cf4e84dec02376bc0013170544692c4698e00
Raheem2030 <raheem.srehini@gmail.com>
2026-03-28 14:26:22 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import WebApp from '@twa-dev/sdk';
 import App from './App.tsx';
 import './index.css';
 
+// إخبار تيليجرام أن التطبيق جاهز تماماً للعرض
 WebApp.ready();
+// إجبار التطبيق على التوسع لملء كامل الشاشة فوراً
+WebApp.expand();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

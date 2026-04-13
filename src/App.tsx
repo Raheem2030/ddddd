0b6fa000452460ae2fe73bcf32033ae662966f35
Raheem2030 <raheem.srehini@gmail.com>
2026-04-13 10:41:32 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Layout } from './components/layout/Layout';
 import { WelcomePage } from './pages/WelcomePage';
 import { SelectionPage } from './pages/SelectionPage';
@@ -9,7 +9,7 @@ import { ChapterPage } from './pages/ChapterPage';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<WelcomePage />} />
@@ -20,6 +20,6 @@ export default function App() {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }

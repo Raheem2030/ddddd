44d255040f9c0525cb70822903d5a58265792144
Raheem2030 <raheem.srehini@gmail.com>
2026-04-25 16:18:53 -0600

    Update index.ts

--- src/data/index.ts
+++ src/data/index.ts
@@ -5,6 +5,7 @@ import { pharmaceutics_1 } from './year2/theoretical/pharmaceutics_1';
 import { analytical_chemistry_1 } from './year2/practical/analytical_chemistry_1';
 import { pharmaceutics_2 } from './year2/practical/pharmaceutics_2';
 import { biochemistry_1 } from './year2/practical/biochemistry_1';
+import { pharmacognosy_1 } from './year2/practical/pharmacognosy_1';
 import { subjects } from './subjects';
 
 export { subjects };
@@ -14,5 +15,6 @@ export const subjectContents: Record<string, SubjectContent> = {
   '2-1-6': pharmaceutics_1,
   '2-2-1': analytical_chemistry_1,
   '2-2-2': pharmaceutics_2,
+  '2-2-4': pharmacognosy_1,
   '2-2-5': biochemistry_1,
 };
